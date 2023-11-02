ee5eb075c937d60d55e9c30fb04970fbf2c516c8
AprilaAditia <aymericaditia.43@gmail.com>
2023-11-02 16:02:35 +0700

    Perubahan

--- resep.php
+++ resep.php
@@ -3,4 +3,7 @@ echo ("RESEP MARTABAK<br>");
 echo("-------------------");
 echo ("1. Bawang<br>");
 echo ("2. Ketimun<br>");
+echo ("3. Roti<br>");
+echo ("4. Keju <br>");
+echo ("5. Tomat <br>");
 ?>
